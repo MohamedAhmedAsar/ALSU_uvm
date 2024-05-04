4b4bca742f2c3b834f5bdd669a26240951c7832b
MohamedAhmedAsar <167991359+MohamedAhmedAsar@users.noreply.github.com>
2024-05-04 17:18:33 +0300

    env

--- code/package/ALSU_env.sv
+++ code/package/ALSU_env.sv
@@ -9,6 +9,27 @@ package ALSU_env_pkg;
         ALSU_scoreboard sb;
         ALSU_coverage cov;
 
+        function new(string name = "ALSU_env", uvm_component parent);
+            super.new(name, parent);
+        endfunction: new
+        
+        function void build_phase(uvm_phase phase);
+            super.build_phase(phase);
+            
+            agt=ALSU_agent::type_id::create("agt",this);
+            sb=ALSU_scoreboard::type_id::create("sb",this);
+            cov=ALSU_coverage::type_id::create("cov",this);
+            
+        endfunction: build_phase
+        
+
+        function void connect_phase(uvm_phase phase);
+            super.connect_phase(phase);
+        
+            agt.agt_ap.connect(sb.sb_export);
+            agt.agt_ap.connect(cov.cov_export);
+
+        endfunction: connect_phase
         
 
     endclass
